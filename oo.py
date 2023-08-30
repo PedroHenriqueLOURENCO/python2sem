3206f5b59aadc037b122d7d7e747fc9704716ca4
pedrohenriquelourenco <rm98402@fiap.com.br>
2023-08-29 22:19:12 -0300

    defs e afins...

--- oo.py
+++ oo.py
@@ -1,9 +1,27 @@
-class inventario:
-    def add():
-        ...
-    def remover():
-        ...
-    def _init_(self):
-        self.preco
-        self.qtd
-        self.dep
+class Inventario:
+    # def add():
+    #     ...
+    # def alterar(self):
+    #     self.__preco = self.__preco*0.9
+    #     ...
+
+    def depreciar(self):
+        self.__preco = self.preco*0.9
+    
+    def get_preco(self):
+        return self.preco
+    def set_preco(self, preco):
+        self.__preco = preco 
+
+    def _init_(self, preco, qtd, dep):
+        self.__preco = preco
+        self.qtd = qtd
+        self.dep = dep
+# __preco = input("oi: ")
+if __name__ == "_main_":
+    inventario1 = Inventario(5000, 2, "Pessoal")
+    print(inventario1.preco)
+    
+    inventario1.depreciar()
+    print(inventario1.preco)
+    
