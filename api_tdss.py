659a0ab03dde42c8a2a072897e1c006d5f300a35
PedroHenriqueLOURENCO <rm98402@fiap.com.br>
2023-09-12 22:19:38 -0300

    sync

--- api_tdss.py
+++ api_tdss.py
@@ -4,15 +4,30 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class main(Resource):
+lista = ["Lucas"]
+
+
+class Listar(Resource):
     def get(self):
-        return "Funciona"
-    def post(self):
-        ...
+        for elemento in lista:
+            dados = elemento + "\n"
+        return str(dados)
+
+class Main (Resource):
+    def post(self, nome):
+        lista.append(str(nome).title())
+        return f'Usuario {nome} foi adicionado com sucesso!'
     def put(self):
         ...
-    def delete(self):
-        ...
+    def delete(self, nome):
+        if nome in lista:
+            for index in range(o, len(lista)):
+                if lista[index] == nome:
+                    del lista[index]
+                    return "elemento removido"
+        else:
+            return "elemento não existe"
+                    
 
 class version(Resource):
     def get(self):
@@ -23,8 +38,8 @@ class version(Resource):
         ...
     def delete(self):
         ...
-
-api.add_resource(main, "/")
+api.add_resource(Listar, "/>")
+api.add_resource(Main, "/<nome>")
 api.add_resource(version, "/version")
 
 if __name__ == "__main__":
