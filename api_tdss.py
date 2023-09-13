8b74cf750b7e3a4f66d0c5e8ff1d379e1d2550fa
PedroHenriqueLOURENCO <rm98402@fiap.com.br>
2023-09-12 22:24:12 -0300

    limpeza

--- api_tdss.py
+++ api_tdss.py
@@ -6,41 +6,30 @@ api = Api(app)
 
 lista = ["Lucas"]
 
-
 class Listar(Resource):
     def get(self):
-        for elemento in lista:
-            dados = elemento + "\n"
-        return str(dados)
-
-class Main (Resource):
+        return str(lista)
+    
+class Main(Resource):
     def post(self, nome):
         lista.append(str(nome).title())
         return f'Usuario {nome} foi adicionado com sucesso!'
-    def put(self):
-        ...
     def delete(self, nome):
         if nome in lista:
-            for index in range(o, len(lista)):
+            for index in range(0, len(lista)):
                 if lista[index] == nome:
                     del lista[index]
-                    return "elemento removido"
+                    return "Elemento removido"
         else:
-            return "elemento não existe"
-                    
+            return "Elemento não existe"
 
-class version(Resource):
+class Version(Resource):
     def get(self):
         return "0.0.1"
-    def post(self):
-        ...
-    def put(self):
-        ...
-    def delete(self):
-        ...
-api.add_resource(Listar, "/>")
+
+api.add_resource(Listar, "/")
 api.add_resource(Main, "/<nome>")
-api.add_resource(version, "/version")
+api.add_resource(Version, "/version")
 
 if __name__ == "__main__":
     app.run(port=8080)
